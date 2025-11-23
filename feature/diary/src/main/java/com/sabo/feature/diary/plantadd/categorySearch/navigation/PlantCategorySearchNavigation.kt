9007da1d1b07337773bb76fb67cdf060e365e6a4
Changyeop Lee <paulus0617@gmail.com>
2025-11-23 15:37:14 +0900

    [FIX] 식물 카테고리 검색 네비게이션 애니메이션 수정

--- feature/diary/src/main/java/com/sabo/feature/diary/plantadd/categorySearch/navigation/PlantCategorySearchNavigation.kt
+++ feature/diary/src/main/java/com/sabo/feature/diary/plantadd/categorySearch/navigation/PlantCategorySearchNavigation.kt
@@ -6,6 +6,10 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
 import com.sabo.core.navigator.model.CategorySearch
+import com.sabo.core.navigator.toolkit.slideInFromEnd
+import com.sabo.core.navigator.toolkit.slideOutToEnd
+import com.sabo.core.navigator.toolkit.zoomIn
+import com.sabo.core.navigator.toolkit.zoomOut
 import com.sabo.feature.diary.plantadd.categorySearch.PlantCategorySearchScreen
 import com.sabo.feature.diary.plantadd.categorySearch.PlantCategorySearchViewModel
 
@@ -25,6 +29,10 @@ fun NavGraphBuilder.categorySearchScreen(
     onClickCategory: (String) -> Unit
 ) {
     composable<CategorySearch>(
+        enterTransition = slideInFromEnd(),
+        exitTransition = zoomOut(),
+        popEnterTransition = zoomIn(),
+        popExitTransition = slideOutToEnd()
     ) { entry ->
         val extra = entry.toRoute<CategorySearch>().keyword
         PlantCategorySearchScreen(
